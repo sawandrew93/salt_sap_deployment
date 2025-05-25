821f81d7ef10af3333ea58640f13faa76f0fc9fc
Andrew <144896298+sawandrew93@users.noreply.github.com>
2025-05-25 17:42:11 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,47 +1,64 @@
 #!/bin/bash
-HANA_USER="SYSTEM"
-HANA_PASSWORD="Passw0rd"
-HANA_HOST="localhost"
-HANA_PORT="30013"
-HANA_DB="NDB"
-NEW_USER="SAPADMIN"
-NEW_PASS="Passw0rd"
-CWD=$(pwd)
+
+# Change values of below variables if you want
+SID="NDB" #Tenant database name
+SYSTEM_USER_PW="Passw0rd" #SYSTEM database user password
+NEW_DB_USER="SAPADMIN" #SAPADMIN or as you want
+NEW_DB_USER_PW="Hana@123" #new db user password
+B1SITEUSER_PW="Hana@123"
+# Change values of above variables if you want
+
+
+
+
+
+SID_USER=$(echo "$SID" | tr '[:upper:]' '[:lower:]')adm
+SAP_ADMIN_PW_UPDATE=$NEW_DB_USER_PW
+SYSTEM_ADMIN_PW_UPDATE=$SYSTEM_USER_PW
+
 #function to check whether database user already exists
-user_exists=$(su - ndbadm -c "echo \"SELECT user_name FROM users WHERE user_name='${NEW_USER}';\" | hdbsql -u ${HANA_USER} -p ${HANA_PASSWORD} -n ${HANA_HOST}:${HANA_PORT} -d ${HANA_DB}" | awk '/^\"SAPADMIN\"$/ {print}' | tr -d '"')
+user_exists=$(
+  su - $SID_USER -c "echo \"SELECT user_name FROM users WHERE user_name='${NEW_DB_USER}';\" | hdbsql -u SYSTEM -p ${SYSTEM_USER_PW} -n localhost:30013 -d ${SID}" \
+  | awk -v user="\"${NEW_DB_USER}\"" '$0 == user { print }' \
+  | tr -d '"'
+)
 
-
+#set log file directory and dowload hana and sap parameters from github repo
 set -e  # Exit on error
 LOGFILE="/var/log/install_script.log"
 curl -O https://raw.githubusercontent.com/sawandrew93/salt_sap_deployment/refs/heads/main/hdb_param.cfg
 curl -O https://raw.githubusercontent.com/sawandrew93/salt_sap_deployment/refs/heads/main/sap_param.cfg
+
+
+#dependency checking
 if zypper lr | grep -q vglocal; then
-    echo "vglocal repository is already enabled"
+    echo "vglocal repository is already enabled" | tee -a "$LOGFILE"
 else
-    echo "Adding local repository..."
+    echo "Adding local repository..." | tee -a "$LOGFILE"
     zypper addrepo -G http://121.54.164.70/15-SP3/ vglocal
 fi
 
-echo "Checking and installing required packages..."
+echo "Checking and installing required packages..." | tee -a "$LOGFILE"
 
 PACKAGES="jq libatomic1 rpm-build xmlstarlet python2-pyOpenSSL bc glibc-i18ndata \
           libcap-progs libicu60_2 insserv-compat nfs-kernel-server"
 
 for pkg in $PACKAGES; do
     if ! rpm -q $pkg &>/dev/null; then
-        echo "Installing $pkg..."
+        echo "Installing $pkg..." | tee -a "$LOGFILE"
         zypper install -y $pkg
     else
-        echo "$pkg is already installed."
+        echo "$pkg is already installed." | tee -a "$LOGFILE"
     fi
 done
 
-echo "Dependency Packages installation complete!"
+echo "Dependency Packages installation complete!" | tee -a "$LOGFILE"
 
 
-echo "Replacing components and updating permissions..."
-#hdb_param_file=$(find / -type f -name "hdb_param.cfg" 2>/dev/null | head -n 1)
-hdb_param_file=$CWD/hdb_param.cfg
+
+#Modifying hdb_param.cfg file before using it as input file and giving exec permission on hana installer directory
+echo "Modifying hdb_param.cfg file and giving exec permissions on hana installer directory..." | tee -a "$LOGFILE"
+hdb_param_file=$(find / -type f -name "hdb_param.cfg" 2>/dev/null | head -n 1)
 cp "$hdb_param_file" /tmp/hdb.cfg
 
 hana_afl_dir=$(find / -type d -name "SAP_HANA_AFL" 2>/dev/null | head -n 1)
@@ -51,64 +68,75 @@ hana_db_dir=$(find / -type d -name "SAP_HANA_DATABASE" 2>/dev/null | head -n 1)
 sed -i "s|hana_afl_dir|${hana_afl_dir}|g" /tmp/hdb.cfg
 sed -i "s|hana_client_dir|${hana_client_dir}|g" /tmp/hdb.cfg
 sed -i "s|hana_db_dir|${hana_db_dir}|g" /tmp/hdb.cfg
+sed -i "s|sap_admin_pw|${SAP_ADMIN_PW_UPDATE}|g" /tmp/hdb.cfg # to change sap_adm_pw inside /tmp/hdb.cfg with update value from variable
+sed -i "s|system_pw|${SYSTEM_ADMIN_PW_UPDATE}|g" /tmp/hdb.cfg # to change system databse user password inside /tmp/hdb.cfg with update value from variable
+sed -i "s|NDB|${SID}|g" /tmp/hdb.cfg # to change sid value inside /tmp/hdb.cfg with update value from variable
 chmod +x -R "$hana_db_dir"
 chmod +x -R "$hana_client_dir"
 chmod +x -R "$hana_afl_dir"
 
-echo "Checking SAP HANA Database installation..."
 
-if su - ndbadm -c "HDB version" &>/dev/null; then
-    echo "SAP HANA is already installed."
-    su - ndbadm -c "HDB version"  # This will display the version info
+#Install hana database, afl and client #It won't be executed if hana is already installed
+echo "Checking SAP HANA Database installation..." | tee -a "$LOGFILE"
+
+if su - $SID_USER -c "HDB version" &>/dev/null; then
+    echo "SAP HANA is already installed." | tee -a "$LOGFILE"
+    su - $SID_USER -c "HDB version"  # This will display the version info
 else
     if [[ -d "$hana_db_dir" ]]; then
         echo "Installing HANA Database services..." | tee -a "$LOGFILE"
         cd "$hana_db_dir"
-        ./hdblcm --batch --configfile="/tmp/hdb.cfg" &>> "$LOGFILE"
+        ./hdblcm --batch --configfile="/tmp/hdb.cfg" 2>&1 | tee -a "$LOGFILE"
         rm /tmp/hdb.cfg
         echo "Installation completed!" | tee -a "$LOGFILE"
     else
-        echo "Cannot find HANA installer directory."
+        echo "Cannot find HANA installer directory." | tee -a "$LOGFILE"
     fi
 fi
 
-
 #DB user creation
-if [[ "$user_exists" == "$NEW_USER" ]]; then
-    echo "User ${NEW_USER} already exists. Skipping creation."
+if [[ "$user_exists" == "$NEW_DB_USER" ]]; then
+    echo "User ${NEW_DB_USER} already exists. Skipping creation."
 else
-    echo "Creating user ${NEW_USER}..."
+    echo "Creating user ${NEW_DB_USER}..."
 
-    su - ndbadm -c "hdbsql -u ${HANA_USER} -p ${HANA_PASSWORD} -n ${HANA_HOST}:${HANA_PORT} -d ${HANA_DB} <<EOF
-CREATE USER ${NEW_USER} PASSWORD \"${NEW_PASS}\" NO FORCE_FIRST_PASSWORD_CHANGE;
-ALTER USER ${NEW_USER} DISABLE PASSWORD LIFETIME;
-GRANT CONTENT_ADMIN TO ${NEW_USER};
-GRANT AFLPM_CREATOR_ERASER_EXECUTE TO ${NEW_USER};
-GRANT \"IMPORT\" TO ${NEW_USER};
-GRANT \"EXPORT\" TO ${NEW_USER};
-GRANT \"INIFILE ADMIN\" TO ${NEW_USER};
-GRANT \"LOG ADMIN\" TO ${NEW_USER};
-GRANT \"CREATE SCHEMA\",\"USER ADMIN\",\"ROLE ADMIN\",\"CATALOG READ\" TO ${NEW_USER} WITH ADMIN OPTION;
-GRANT \"CREATE ANY\",\"SELECT\" ON SCHEMA \"SYSTEM\" TO ${NEW_USER} WITH GRANT OPTION;
-GRANT \"SELECT\",\"EXECUTE\",\"DELETE\" ON SCHEMA \"_SYS_REPO\" TO ${NEW_USER} WITH GRANT OPTION;
+    su - $SID_USER -c "hdbsql -u SYSTEM -p ${SYSTEM_USER_PW} -n localhost:30013 -d ${SID} <<EOF
+CREATE USER ${NEW_DB_USER} PASSWORD \"${NEW_DB_USER_PW}\" NO FORCE_FIRST_PASSWORD_CHANGE;
+ALTER USER ${NEW_DB_USER} DISABLE PASSWORD LIFETIME;
+GRANT CONTENT_ADMIN TO ${NEW_DB_USER};
+GRANT AFLPM_CREATOR_ERASER_EXECUTE TO ${NEW_DB_USER};
+GRANT \"IMPORT\" TO ${NEW_DB_USER};
+GRANT \"EXPORT\" TO ${NEW_DB_USER};
+GRANT \"INIFILE ADMIN\" TO ${NEW_DB_USER};
+GRANT \"LOG ADMIN\" TO ${NEW_DB_USER};
+GRANT \"CREATE SCHEMA\",\"USER ADMIN\",\"ROLE ADMIN\",\"CATALOG READ\" TO ${NEW_DB_USER} WITH ADMIN OPTION;
+GRANT \"CREATE ANY\",\"SELECT\" ON SCHEMA \"SYSTEM\" TO ${NEW_DB_USER} WITH GRANT OPTION;
+GRANT \"SELECT\",\"EXECUTE\",\"DELETE\" ON SCHEMA \"_SYS_REPO\" TO ${NEW_DB_USER} WITH GRANT OPTION;
 EOF"
 
     if [[ $? -eq 0 ]]; then
-        echo "User ${NEW_USER} created and privileges granted successfully."
+        echo "User ${NEW_DB_USER} created and privileges granted successfully."
     else
-        echo "Error creating user ${NEW_USER}."
+        echo "Error creating user ${NEW_DB_USER}."
         exit 1
     fi
 fi
 
-#sap prerequisites preparing
+#Modifying sap_param.cfg file before using it as input file and giving exec permission on sap installer directory
 echo "Configuring SAP installation prerequisites..."
         sap_dir=$(find / -type d -name "ServerComponents" 2>/dev/null | head -n 1)
+        if [[ -z "$sap_dir" ]]; then
+            echo "Error: SAP Installer directory not found!"
+            exit 1
+        fi
         chmod +x -R "$sap_dir"
-        #sap_param_file=$(find / -type f -name "sap_param.cfg" 2>/dev/null | head -n 1)
-        sap_param_file=$CWD/sap_param.cfg
+        sap_param_file=$(find / -type f -name "sap_param.cfg" 2>/dev/null | head -n 1)
         cp "$sap_param_file" /tmp/sap.cfg
         sed -i "s/serverfqdn/$(hostname)/g" /tmp/sap.cfg
+        sed -i "s/NDB/${SID}/g" /tmp/sap.cfg
+        sed -i "s/SAPADMIN/${NEW_DB_USER}/g" /tmp/sap.cfg
+        sed -i "s/SAPADMIN_PW/${NEW_DB_USER_PW}/g" /tmp/sap.cfg
+        sed -i "s/B1SITEUSER_PW/${B1SITEUSER_PW}/g" /tmp/sap.cfg
 
 #install SAP
 echo "Checking SAP installation status..." | tee -a "$LOGFILE"
@@ -119,7 +147,7 @@ else
     echo "Starting SAP installation..." | tee -a "$LOGFILE"
     if [[ -d "$sap_dir" ]]; then
         cd "$sap_dir"
-        ./install -i silent -f /tmp/sap.cfg &>> "$LOGFILE"
+        ./install -i silent -f /tmp/sap.cfg --debug 2>&1 | tee -a "$LOGFILE"
         echo "SAP installation completed!" | tee -a "$LOGFILE"
         rm /tmp/sap.cfg
     else
