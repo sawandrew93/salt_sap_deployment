7a7e4f13d51b89d8ddc915f19972dd74c1d2dc61
Andrew <144896298+sawandrew93@users.noreply.github.com>
2025-05-26 09:12:43 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Change values of below variables if you want
-SID="ABC" #Tenant database name
+SID="NDB" #Tenant database name
 SYSTEM_USER_PW="Passw0rd" #SYSTEM database user password
-NEW_DB_USER="VGADMIN" #SAPADMIN or as you want
-NEW_DB_USER_PW="V@nguard@929" #new db user password
+NEW_DB_USER="SAPADMIN" #SAPADMIN or as you want
+NEW_DB_USER_PW="H@na@123" #new db user password
 B1SITEUSER_PW="Hana@123"
 # Change values of above variables if you want
 
@@ -13,7 +13,6 @@ B1SITEUSER_PW="Hana@123"
 
 
 SID_USER=$(echo "$SID" | tr '[:upper:]' '[:lower:]')adm
-
 
 #function to check whether database user already exists
 user_exists=$(
@@ -70,9 +69,24 @@ sed -i "s|hana_db_dir|${hana_db_dir}|g" /tmp/hdb.cfg
 sed -i "s|sap_admin_pw|${NEW_DB_USER_PW}|g" /tmp/hdb.cfg # to change sap_adm_pw inside /tmp/hdb.cfg with update value from variable
 sed -i "s|system_pw|${SYSTEM_USER_PW}|g" /tmp/hdb.cfg # to change system databse user password inside /tmp/hdb.cfg with update value from variable
 sed -i "s|NDB|${SID}|g" /tmp/hdb.cfg # to change sid value inside /tmp/hdb.cfg with update value from variable
-chmod +x -R "$hana_db_dir"
-chmod +x -R "$hana_client_dir"
-chmod +x -R "$hana_afl_dir"
+if [[ -d "$hana_db_dir" ]]; then
+    chmod +x -R "$hana_db_dir"
+else
+    echo "Cannot find HANA database installer directory." | tee -a "$LOGFILE"
+fi
+
+if [[ -d "$hana_client_dir" ]]; then
+    chmod +x -R "$hana_client_dir"
+else
+    echo "Cannot find HANA database client installer directory." | tee -a "$LOGFILE"
+fi
+
+if [[ -d "$hana_afl_dir" ]]; then
+    chmod +x -R "$hana_afl_dir"
+else
+    echo "Cannot find HANA AFL installer directory." | tee -a "$LOGFILE"
+fi
+
 
 
 #Install hana database, afl and client #It won't be executed if hana is already installed
@@ -80,24 +94,28 @@ echo "Checking SAP HANA Database installation..." | tee -a "$LOGFILE"
 
 if su - $SID_USER -c "HDB version" &>/dev/null; then
     echo "SAP HANA is already installed." | tee -a "$LOGFILE"
-    su - $SID_USER -c "HDB version"  # This will display the version info
+    su - $SID_USER -c "HDB version"  # This will echo current installed version 
 else
     if [[ -d "$hana_db_dir" ]]; then
         echo "Installing HANA Database services..." | tee -a "$LOGFILE"
         cd "$hana_db_dir"
         ./hdblcm --batch --configfile="/tmp/hdb.cfg" 2>&1 | tee -a "$LOGFILE"
-        echo "Installation completed!" | tee -a "$LOGFILE"
+        if [[ $? -eq 0 ]]; then
+            echo "Installation completed successfully!" | tee -a "$LOGFILE"
+        else
+            echo "Installation failed!" | tee -a "$LOGFILE"
+        fi       
     else
         echo "Cannot find HANA installer directory." | tee -a "$LOGFILE"
     fi
 fi
 
 
-#DB user creation
+#HANA DB user creation
 if [[ "$user_exists" == "$NEW_DB_USER" ]]; then
     echo "User ${NEW_DB_USER} already exists. Skipping creation."
 else
-    echo "Creating user ${NEW_DB_USER}..."
+    echo "Creating user ${NEW_DB_USER}..." | tee -a "$LOGFILE"
 
     su - $SID_USER -c "hdbsql -u SYSTEM -p ${SYSTEM_USER_PW} -n localhost:30013 -d ${SID} <<EOF
 CREATE USER ${NEW_DB_USER} PASSWORD \"${NEW_DB_USER_PW}\" NO FORCE_FIRST_PASSWORD_CHANGE;
@@ -114,9 +132,9 @@ GRANT \"SELECT\",\"EXECUTE\",\"DELETE\" ON SCHEMA \"_SYS_REPO\" TO ${NEW_DB_USER
 EOF"
 
     if [[ $? -eq 0 ]]; then
-        echo "User ${NEW_DB_USER} created and privileges granted successfully."
+        echo "User ${NEW_DB_USER} created and privileges granted successfully." | tee -a "$LOGFILE"
     else
-        echo "Error creating user ${NEW_DB_USER}."
+        echo "Error creating hana database user ${NEW_DB_USER}." | tee -a "$LOGFILE"
         exit 1
     fi
 fi
@@ -132,7 +150,6 @@ else
         echo "SAP installer directory not found!" | tee -a "$LOGFILE"
         exit 1
 fi
-chmod +x -R "$sap_dir"
 sap_param_file=$(find / -type f -name "sap_param.cfg" 2>/dev/null | head -n 1)
 if [ -z "$sap_param_file" ]; then
     echo "Error: 'sap_param.cfg' file not found." | tee -a "$LOGFILE"
@@ -163,6 +180,8 @@ else
         exit 1
     fi
 fi
+
+#remove config files after installation
 rm hdb_param.cfg
 rm sap_param.cfg
 rm /tmp/sap.cfg
